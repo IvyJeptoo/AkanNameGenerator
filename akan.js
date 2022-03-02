b72d3ddf68f09e6ceb5dcfad2860ee4f6dbb88e4
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-03-03 00:35:03 +0300

    create checkday function and initialize variables

--- akan.js
+++ akan.js
@@ -3,3 +3,14 @@ let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let femaleNames = ["Akosue", "Adwoa", "Abenaa", "Akue", "Yaa", "Afua", "Ama"]
 let genders = document.getElementsByClassName("gender")
 
+function checkDay(x) {
+    let isValidDate = Date.parse(x);
+    let date = new Date(isValidDate);
+    let index = date.getDay();
+    let z = document.getElementById("message");
+    z.innerHTML = (`Hi! you were born on a ${days[index]} and your Akan name is ${femaleNames[index]}.`)
+}
+
+function displayDate(){
+
+}
